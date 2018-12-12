9a7a005cf886978ac0d2cde1f594bc78d51f30ec
Jonas kervin Hansen <jonas.kerwin.hansen@gmail.com>
2018-12-12 19:16:30 +0100

    Removed unused bindings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,6 @@
 
 namespace App\Providers;
 
-use App\Contracts\Emulator;
-use App\Contracts\Emulators\GathersGameStatistics;
-use App\Contracts\Emulators\ResolvesDatabaseConnections;
-use App\Contracts\Emulators\SendsIngameMails;
-use App\Emulators\EmulatorManager;
 use App\Enums\UserType;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Blade;
@@ -37,29 +32,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->registerEmulatorBindings();
-    }
-
-    public function registerEmulatorBindings()
-    {
-        $this->app->singleton(EmulatorManager::class, function ($app) {
-            return new EmulatorManager($app);
-        });
-
-        $this->app->bind(Emulator::class, function ($app) {
-            return $app[EmulatorManager::class]->driver();
-        });
-
-        $this->app->bind(SendsIngameMails::class, function ($app) {
-            return Mail::makeWithEmulator($app[Emulator::class]);
-        });
-
-        $this->app->bind(ResolvesDatabaseConnections::class, function ($app) {
-            return $app[Emulator::class]->database();
-        });
-
-        $this->app->bind(GathersGameStatistics::class, function ($app) {
-            return $app[Emulator::class]->statistics();
-        });
+        //
     }
 }
